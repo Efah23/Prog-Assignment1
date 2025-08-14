8d7615899f47a5243ccac4d4060bd2600a834f09
Efah23 <ST10470414@rcconnect.edu.za>
2025-08-14 22:02:19 +0200

    assignment

---
+++ src/SeriesModel.java
@@ -0,0 +1,13 @@
+public class SeriesModel {
+    public String seriesId;
+    public String seriesName;
+    public String seriesAge;
+    public String seriesNumberOfEpisodes;
+
+    public SeriesModel(String seriesId, String seriesName, String seriesAge, String seriesNumberOfEpisodes) {
+        this.seriesId = seriesId;
+        this.seriesName = seriesName;
+        this.seriesAge = seriesAge;
+        this.seriesNumberOfEpisodes = seriesNumberOfEpisodes;
+    }
+}
